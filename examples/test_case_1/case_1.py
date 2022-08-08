2348183d95be3076671b81eae1de1e72240f45b4
Darren Lynch <dlynch@simscale.com>
2022-08-08 09:48:01 +0100

    Improved example

--- examples/test_case_1/case_1.py
+++ examples/test_case_1/case_1.py
@@ -1,17 +1,13 @@
 from topo_extend.Topo_extend_v2 import topology
 import pathlib
 
-origin = [99.5, 101, 0]
-extension = 500
-resolution = 1
-
 input_path = pathlib.Path('test_case_1.stl')
 output_path = pathlib.Path('TOPOLOGY_EXTENSION.stl')
 
-mesh_clean = topology(origin=origin,
-                      resolution=resolution)
+mesh_clean = topology(origin=[99.5, 101, 0],
+                      resolution=1)
 
 mesh_clean.extend_stl(input_path,
                       output_path,
-                      extension_radius=extension,
+                      extension_radius=500,
                       inclusion_radius=50)
