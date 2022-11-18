a5820b450fb0fc8466f67aa74cdf9f07b6315f44
Darren Lynch <dlynch@simscale.com>
2022-11-18 14:23:48 +0000

    update

--- examples/test_case_1/case_1.py
+++ examples/test_case_1/case_1.py
@@ -12,13 +12,10 @@ mesh_clean = topology(origin=[99.5, 101, 0],
                       resolution=0.5)
 
 mesh_clean.extend_stl(input_path,
-                      output_path,
                       extension_radius=500,
                       inclusion_radius=50)
 
+mesh_clean.export_reduced_mesh(output_path)
+
+#Return the number of triangles in the mesh
 no_triangles = mesh_clean.get_no_triangles()
-'''
-mesh_clean.reduce_mesh_target_size(50) #Megabytes
-no_triangles = mesh_clean.get_no_triangles()
-print('After mesh reduction: {}'.format(no_triangles))
-'''
