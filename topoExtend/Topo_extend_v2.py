300fc0a28b9d693e18db0ee12d2e5318d201304c
Darren Lynch <dlynch@simscale.com>
2022-11-21 10:42:55 +0000

    Update Topo_extend_v2.py

--- topoExtend/Topo_extend_v2.py
+++ topoExtend/Topo_extend_v2.py
@@ -534,7 +534,7 @@ class topology():
         
         cell_radius = (cell_centres[:, 0]**2 + cell_centres[:, 1]**2)**0.5
         farfield_cell_idx = np.transpose(np.nonzero(cell_radius>self.disc_radius))
-        farfield_cells = remesh.extract_cells(farfield_cell_idx)
+        farfield_cells = remesh.extract_cells(farfield_cell_idx).extract_surface()
         print(farfield_cells)
         recentered = farfield_cells.translate(self.origin, inplace=True)
         recentered.save(output_path, 
